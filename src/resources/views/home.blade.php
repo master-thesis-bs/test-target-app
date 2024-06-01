db60dee06954d9d6aa4179b80c1b577c67286172
starzyk.bartlomiej <starzyk.bartlomiej@gmail.com>
2024-06-01 20:41:16 +0200

    update home page

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -1,7 +1,11 @@
 <x-body>
     <x-slot name="content">
         <div style="text-align: center">
-            </h1><br><br><br><br><h1 style="color:white">Home</h1><br><br><br><br>
+            <br><br><br><br><h1 style="color:white">Home</h1><br>
+            <p style="color:white; padding-left:200px; padding-right:200px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consectetur libero in sem scelerisque condimentum. Nunc porta sapien at ultricies tincidunt. Sed eget tellus augue. Nam vitae est eget mauris efficitur convallis a in felis. Praesent hendrerit pharetra cursus. Donec quam velit, volutpat vel velit et, malesuada mollis odio. Vivamus metus dolor, luctus sit amet enim at, egestas pellentesque velit.<p/>
+            <p style="color:white; padding-left:200px; padding-right:200px;">In sodales justo lacus, vel tempor quam lobortis eget. Fusce sed pellentesque libero. Morbi in elit nunc. In sit amet quam quis nisi rutrum varius. Quisque tempus mollis justo ut finibus. Curabitur et sapien ultricies, pharetra ante non, tincidunt sem. Sed laoreet enim nisi. Vestibulum sagittis ut urna vitae scelerisque. Sed ultrices tellus eget vestibulum dignissim. Proin auctor malesuada interdum. Etiam posuere sollicitudin dictum. Vestibulum nulla ante, auctor vitae imperdiet eu, facilisis vel urna.<p/>
+            <p style="color:white; padding-left:200px; padding-right:200px;">Vestibulum bibendum consequat placerat. Cras dapibus quam eros, id congue arcu vehicula sit amet. Suspendisse semper imperdiet elit eget molestie. Quisque rutrum ante non libero accumsan semper. Sed id aliquam massa, vitae molestie enim. Nulla facilisi. Aenean molestie consequat magna at tempus. Aenean malesuada odio consectetur mattis semper. Fusce eu neque dui. In fermentum egestas lorem, eget lacinia odio imperdiet sit amet. Vivamus cursus odio sit amet felis varius convallis. Aenean vitae ante at augue luctus dignissim. Vestibulum egestas ex a orci sodales suscipit. Proin dignissim magna quis massa commodo ultrices. Sed fermentum nisl et fermentum tincidunt.<p/>
+            <br><br><br><br><br>
         </div>
     </x-slot>
 </x-body>
